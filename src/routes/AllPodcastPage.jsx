98ec64b20dfd430268e326a1a3b5cd2fee15628f
Ivan Sanchez Leyva <deepman42@protonmail.com>
2023-04-08 15:04:16 +0200

    styles

---
+++ src/routes/AllPodcastPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import AllPodcast from "../components/AllPodcast/AllPodcast";
+
+const AllPodcastPage = () => {
+  return <AllPodcast />;
+  //More components might be needed in a future like trending podcast or footer;
+};
+
+export default AllPodcastPage;
